d4a734cacbc22a74f6605b29b1652708279b4f38
Andre Pilikin <andr1312e@outlook.com>
2020-02-27 21:37:04 +0300

    Добавьте файлы проекта.

---
+++ Paterns from ITVDN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paterns_from_ITVDN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
